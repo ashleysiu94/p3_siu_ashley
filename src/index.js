a495d4979c9a9bf905dcdc6856c9afca3b76aec2
Ashley Siu <ashley.c.siu@gmail.com>
2020-12-08 09:58:55 -0800

    code for side dish random generator

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ class App {
   constructor(){
     this.renderTemplate();
     main_generator.changeImage();
+    side_generator.changeImage();
   }
 
   renderTemplate() {
@@ -23,7 +24,7 @@ class App {
       <main>
         <div id="food">
           <div>
-            <img id="side" src="../images/guac_side_mexican.png" alt="guacamole" title="guacamole">
+            <img id="side" src="${side_generator.photo}" alt="${side_generator.name}" title="${side_generator.name}">
             <img id="drink" src="../images/lemonade_drink_american.png" alt="lemonade" title="lemonade">
           </div>
           <img id="main" src="${main_generator.photo}" title="${main_generator.name}" alt="${main_generator.name}">
@@ -33,7 +34,7 @@ class App {
           <hr>
           <ul>
             <li>${menu.main}<span id="main-name">${main_generator.name}</span></li>
-            <li>${menu.side}</li>
+            <li>${menu.side}<span id="side-name">${side_generator.name}</span></li>
             <li>${menu.drink}</li>
           </ul>
           <p>${menu.prompt}</p>
--- src/modules/side-generator.js
+++ src/modules/side-generator.js
@@ -4,11 +4,23 @@ class SideGenerator {
     this.photo = photo;
     this.type = type;
   }
+
+  changeImage(){
+    document.getElementById("side").addEventListener('click', ()=>{
+      const selectedDish = sideDishes[Math.floor(Math.random()*sideDishes.length)];
+      document.getElementById("side").src = selectedDish.photo;
+      document.getElementById("side").alt = selectedDish.name;
+      document.getElementById("side").title = selectedDish.name;
+      document.getElementById("side-name").innerHTML = selectedDish.name;
+    });
+  }
 }
 
 const kimchi = new SideGenerator('Kimchi','../images/kimchi_side_korean.png','Korean');
-const salad = new SideGenerator('Side','../images/salad_side_american.png','American');
+const salad = new SideGenerator('Salad','../images/salad_side_american.png','American');
 const guacamole = new SideGenerator('Guacamole','../images/guac_side_mexican.png','Mexican');
 const minestrone = new SideGenerator('Minestrone','../images/minestrone_side_italian.png','Italian');
 
-export const side_generator = new SideGenerator('Lunch Side Dish Content!');
+let sideDishes = [kimchi, salad, guacamole, minestrone];
+
+export const side_generator = sideDishes[Math.floor(Math.random()*sideDishes.length)];
